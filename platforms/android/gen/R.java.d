40185d4a98370fe7e6f0c32c45032fab54811f1d
Kieranties <Kieran@Kieranties.com>
2013-08-12 17:20:58 +0100

    Updated index.js location

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,14 @@
+C:\Users\gokulives\Dropbox\Projects\Ashtag\platforms\android\gen\org\iasess\ashtag\R.java \
+ : C:\Users\gokulives\Dropbox\Projects\Ashtag\platforms\android\res\drawable\icon.png \
+C:\Users\gokulives\Dropbox\Projects\Ashtag\platforms\android\res\drawable-hdpi\icon.png \
+C:\Users\gokulives\Dropbox\Projects\Ashtag\platforms\android\res\drawable-hdpi\ic_launcher.png \
+C:\Users\gokulives\Dropbox\Projects\Ashtag\platforms\android\res\drawable-ldpi\icon.png \
+C:\Users\gokulives\Dropbox\Projects\Ashtag\platforms\android\res\drawable-ldpi\ic_launcher.png \
+C:\Users\gokulives\Dropbox\Projects\Ashtag\platforms\android\res\drawable-mdpi\icon.png \
+C:\Users\gokulives\Dropbox\Projects\Ashtag\platforms\android\res\drawable-mdpi\ic_launcher.png \
+C:\Users\gokulives\Dropbox\Projects\Ashtag\platforms\android\res\drawable-xhdpi\icon.png \
+C:\Users\gokulives\Dropbox\Projects\Ashtag\platforms\android\res\drawable-xhdpi\ic_launcher.png \
+C:\Users\gokulives\Dropbox\Projects\Ashtag\platforms\android\res\layout\main.xml \
+C:\Users\gokulives\Dropbox\Projects\Ashtag\platforms\android\res\values\strings.xml \
+C:\Users\gokulives\Dropbox\Projects\Ashtag\platforms\android\res\xml\config.xml \
+C:\Users\gokulives\Dropbox\Projects\Ashtag\platforms\android\bin\AndroidManifest.xml \
